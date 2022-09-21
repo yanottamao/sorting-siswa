8ba2c3ef02d3c23d638b62d64a4a45ce7aa3f3d1
Yanottama Oktabrian <oktatama4@gmail.com>
2022-09-21 14:09:58 +0700

    feat: menambah instruksi

--- home.php
+++ home.php
@@ -21,7 +21,8 @@ $userID = $_SESSION['userID'];
     <div id="body">
         <div id="content" class="text-center">
             <h1>Selamat Datang <?php echo $username; ?></h1>
-            <h3 style="color:blue;"><?php echo "Hari ini " .  "  " . date("d-F-Y") . " - " . date("l") . "" ?>&nbsp; <?php echo "" . date("G:i:s e") . "" ?></h3>
+            <p class="fs-4">Silakan pilih menu dari navigasi di atas.</p>
+            <h3 style="color:blue;"><?php echo "Hari ini " .  "  " . date("d F Y") . " - " . date("l") . "" ?>&nbsp; <?php echo "" . date("G:i:s e") . "" ?></h3>
 
             <hr style="border:1px solid red;">
 
